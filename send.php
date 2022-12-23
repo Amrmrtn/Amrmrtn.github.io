7b6ad1fa9a7d1280db2c5516d7fd91566b99918c
Amrmrtn <martinoammar4@gmail.com>
2022-12-24 00:33:36 +0700

    first commit

---
+++ send.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <form action="send.php" method="post"></form>
+    Nama Lengkap: <input type="text" nama="fnama"/>
+    <br/>
+    Jenis Kelamin: <input type="text" nama="fjenis"/>
+    <br/>
+    Alamat Tinggal: <input type="text" nama="falamat"/>
+    <br/>
+    Email: <input type="text" nama="femail"/>
+    <br/>
+    Aktif Pada Social Media: <input type="text" nama="fsocmed"/>
+    <br/>
+    Mengetahui Web Ini Dari: <input type="text" nama="fdari"/>
+    <br/>
+    Pesan: <input type="text" nama="fpesan"/>
+    <br/>
+    <input type="submit" />
+</head>
+</html>
